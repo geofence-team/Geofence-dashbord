f013b82e661bce2ba96b09ded6c7497d16e0d62b
S.Noman <sumayanoman@gmail.com>
2022-07-26 15:26:48 +0300

    deleted signin and join request from dashbord

--- src/routes.js
+++ src/routes.js
@@ -72,16 +72,6 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "Join Request",
-    key: "Join-Request",
-    icon: <Icon fontSize="small">storefront</Icon>,
-    route: "/Join-Request",
-    component: <JoinRequest />,
-    requiresAuth: true,
-    inSideNav: true,
-  },
-  {
-    type: "collapse",
     name: "My Geofences",
     key: "My-Geofences",
     icon: <Icon fontSize="small">storefront</Icon>,
