8ee99f4a1faf2e0b77a7424c384d3b4b415f5c32
S.Noman <sumayanoman@gmail.com>
2022-07-26 15:33:00 +0300

    deleted a menue

--- src/routes.js
+++ src/routes.js
@@ -70,16 +70,6 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "Join Request",
-    key: "Join-Request",
-    icon: <Icon fontSize="small">storefront</Icon>,
-    route: "/Join-Request",
-    component: <JoinRequest />,
-    requiresAuth: true,
-    inSideNav: true,
-  },
-  {
-    type: "collapse",
     name: "My Geofences",
     key: "My-Geofences",
     icon: <Icon fontSize="small">storefront</Icon>,
